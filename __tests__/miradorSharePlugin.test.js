ddabcbac7ec8102b1c4be7a9211cad842203d4aa
Jack Reed <phillipjreed@gmail.com>
2019-05-31 16:36:36 -0600

    Port over basic button and dialog structure

--- __tests__/miradorSharePlugin.test.js
+++ __tests__/miradorSharePlugin.test.js
@@ -17,7 +17,46 @@ describe('miradorSharePlugin', () => {
   describe('renders a component', () => {
     it('renders a thing', () => {
       const wrapper = createWrapper();
-      expect(wrapper.find('h2').text()).toEqual('Share');
+      expect(wrapper.find('WithStyles(ListItemText)').props().children).toEqual('Share');
+    });
+  });
+
+  describe('MenuItem', () => {
+    it('udpates the modalDisplayed state which clicked', () => {
+      const wrapper = createWrapper();
+      expect(wrapper.state().modalDisplayed).toBe(false);
+      wrapper.find('WithStyles(MenuItem)').simulate('click');
+      expect(wrapper.state().modalDisplayed).toBe(true);
+    });
+  });
+
+  describe('Dialog', () => {
+    it('renders a dialog that is open/closed based on the component state', () => {
+      const wrapper = createWrapper();
+      expect(wrapper.state().modalDisplayed).toBe(false);
+      expect(wrapper.find('WithStyles(Dialog)').props().open).toBe(false);
+      wrapper.setState({ modalDisplayed: true });
+      expect(wrapper.find('WithStyles(Dialog)').props().open).toBe(true);
+    });
+
+    it('renders the section headings in an h3', () => {
+      const wrapper = createWrapper();
+      expect(wrapper.find('WithStyles(Typography)[variant="h3"]').get(0).props.children).toEqual('Share link');
+      expect(wrapper.find('WithStyles(Typography)[variant="h3"]').get(1).props.children).toEqual('Alternate viewer');
+      expect(wrapper.find('WithStyles(Typography)[variant="h3"]').get(2).props.children).toEqual('Embed');
+    });
+
+    it('has a close button that updates the modalDisplay state to false', () => {
+      const wrapper = createWrapper();
+      wrapper.setState({ modalDisplayed: true });
+      expect(wrapper.state().modalDisplayed).toBe(true);
+      wrapper.find('WithStyles(Button)').simulate('click');
+      expect(wrapper.state().modalDisplayed).toBe(false);
+    });
+
+    it('has dividers', () => {
+      const wrapper = createWrapper();
+      expect(wrapper.find('WithStyles(Divider)').length).toEqual(3);
     });
   });
 });
